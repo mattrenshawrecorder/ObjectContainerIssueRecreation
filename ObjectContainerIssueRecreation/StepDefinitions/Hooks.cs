51b7184e32b8cc28e5832502bd9285b80c357662
Matt Renshaw <matt.renshaw@recorder.tech>
2024-06-03 13:29:46 +0100

    Add project files.

---
+++ ObjectContainerIssueRecreation/StepDefinitions/Hooks.cs
@@ -0,0 +1,16 @@
+﻿using ObjectContainerIssueRecreation.Drivers;
+using Reqnroll.BoDi;
+
+namespace ObjectContainerIssueRecreation.StepDefinitions
+{
+    [Binding]
+    public class Hooks
+    {
+        [BeforeTestRun]
+        public static void BeforeTestRun(ObjectContainer objectContainer)
+        {
+            // Exception thrown on this line
+            objectContainer.BaseContainer.RegisterInstanceAs(new Driver());
+        }
+    }
+}
